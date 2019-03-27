e38b87f4feac5dfdb0b685c549cefb2a8ee024ed
silvino00 <47743896+silvino00@users.noreply.github.com>
2019-03-27 21:09:31 +0100

    Punch and jump finished

--- Samurai_Shodown/ModulePlayer.cpp
+++ Samurai_Shodown/ModulePlayer.cpp
@@ -70,82 +70,56 @@ ModulePlayer::ModulePlayer()
 	backward.PushBack({ 1351,614,74,114 });
 	backward.speed = 0.4f;
 
-	//backward animation 
-	{
-		backward.PushBack({ 15,614,74,114 });  	//add intermediate frames
-		backward.PushBack({ 94,614,74,114 });
-		backward.PushBack({ 173,614,74,114 });
-		backward.PushBack({ 264,610,68,118 });
-		backward.PushBack({ 337,610,68,118 });
-		backward.PushBack({ 410,612,68,117 });
-		backward.PushBack({ 482,609,67,121 });
-		backward.PushBack({ 553,608,67,121 });
-		/*backward.PushBack({ 624,608,82,121 }); //error
-		backward.PushBack({ 711,608,82,121 });
-		backward.PushBack({ 798,608,82,121 });
-		backward.PushBack({ 884,608,82,121 });
-		backward.PushBack({ 971,608,69,120 });
-		backward.PushBack({ 1045,611,69,117 });
-		backward.PushBack({ 1119,610,69,118 });
-		backward.PushBack({ 1193,614,74,114 });
-		backward.PushBack({ 1272,614,74,114 });
-		backward.PushBack({ 1351,614,74,114 });*/
-		backward.speed = 0.2f;
-	}
-
-
 	//jump animation
-	/*
-	jump.PushBack({16,371,60,112});
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
-	jump.PushBack({ ,,, });
+	
+	jump.PushBack({16, 371, 60, 112});
+	jump.PushBack({79, 371, 60, 112});
+	jump.PushBack({145, 371, 60, 112});
+	jump.PushBack({209, 371, 75, 92});
+	jump.PushBack({288, 371, 75, 92});
+	jump.PushBack({367, 371, 75, 92});
+	jump.PushBack({446, 371, 78, 83});
+	jump.PushBack({527, 371, 68, 112});
+	jump.PushBack({601, 371, 68, 112});
+	jump.PushBack({675, 371, 68, 112});
 	jump.speed = 0.2f;
-	*/
+	
 
 	//punch animation
 	
 	punch.PushBack({ 14, 236, 69, 127 });
 	punch.PushBack({ 87, 236, 69, 127 });
 	punch.PushBack({ 160, 266, 79, 97 });
-	punch.PushBack({ 160, 266, 79, 97 });
 	punch.PushBack({ 243, 269, 79, 96 });
 	punch.PushBack({ 327, 267, 79, 97 });
 	punch.PushBack({ 410, 270, 129, 93 });
 	punch.PushBack({ 545, 277, 130, 86 });
 	punch.PushBack({ 679, 280, 130, 83 });
-	punch.PushBack({ 814, 280, 129, 83 });
-	punch.PushBack({ 946, 280, 130, 83 });
-	punch.PushBack({ 1080, 280, 130, 83 });
-	punch.PushBack({ 1213, 280, 120, 83 });
-	punch.PushBack({ 1336, 280, 120, 83 });
-	punch.PushBack({ 1461, 280, 83, 83 });
-	punch.PushBack({ 1547, 280, 83, 83 });
-	punch.PushBack({ 1634, 280, 83, 83 });
+	punch.PushBack({ 814, 280, 129, 95 });
+	punch.PushBack({ 946, 280, 130, 95 });
+	punch.PushBack({ 1080, 280, 130, 95 });
+	punch.PushBack({ 1213, 280, 120, 95 });
+	punch.PushBack({ 1336, 280, 120, 95 });
+	punch.PushBack({ 1461, 280, 83, 95 });
+	punch.PushBack({ 1547, 280, 83, 95 });
+	punch.PushBack({ 1634, 280, 83, 95 });
 	punch.PushBack({ 1721, 277, 130, 86 });
 	punch.PushBack({ 1857, 277, 130, 86 });
 	punch.speed = 0.2f;
 	
 
 	//kick animation
-	{
-		kick.PushBack({ 16,137,66,95 }); //initial kick frames
-		kick.PushBack({ 87,137,66,95 });
-		kick.PushBack({ 158,137,66,95 });
-		kick.PushBack({ 229,134,86,98 }); //final kick frames
-		kick.PushBack({ 320,134,86,98 });
-		kick.PushBack({ 411,134,86,98 });
-		kick.PushBack({ 16,137,66,95 }); //initial kick frames
-		kick.PushBack({ 87,137,66,95 });
-		kick.PushBack({ 158,137,66,95 });
-		kick.speed = 0.2f;
-	}
+	kick.PushBack({ 16,137,66,95 }); //initial kick frames
+	kick.PushBack({ 87,137,66,95 });
+	kick.PushBack({ 158,137,66,95 });
+	kick.PushBack({ 229,134,86,98 }); //final kick frames
+	kick.PushBack({ 320,134,86,98 });
+	kick.PushBack({ 411,134,86,98 });
+	kick.PushBack({ 16,137,66,95 }); //initial kick frames
+	kick.PushBack({ 87,137,66,95 });
+	kick.PushBack({ 158,137,66,95 });
+	kick.speed = 0.2f;
+	
 }
 
 ModulePlayer::~ModulePlayer(){
