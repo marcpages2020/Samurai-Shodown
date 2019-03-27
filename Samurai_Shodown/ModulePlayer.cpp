43de0c3b28539eaa5d1923173d51390af8eb960f
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-03-27 12:39:57 +0100

    kick

--- Samurai_Shodown/ModulePlayer.cpp
+++ Samurai_Shodown/ModulePlayer.cpp
@@ -48,25 +48,25 @@ ModulePlayer::ModulePlayer()
 	forward.speed = 0.4f;
 
 	//backward animation
-	backward.PushBack({15,614,74,114});
-	backward.PushBack({ 94,614,74,114 });
-	backward.PushBack({ 173,614,74,114 });
-	backward.PushBack({ 264,610,68,118 });
-	backward.PushBack({ 337,610,68,118 });
-	backward.PushBack({ 410,611,68,117 });
-	backward.PushBack({ 486,609,62,110 });
-	backward.PushBack({ 554,608,65,110 });
-	backward.PushBack({ 624,618,82,110 });
-	backward.PushBack({ 711,618,82,110 });
-	backward.PushBack({ 798,618,82,110 });
-	backward.PushBack({ 884,618,82,110 });
-	backward.PushBack({ 971,611,69,117 });
-	backward.PushBack({ 1045,611,69,117 });
-	backward.PushBack({ 1119,610,69,118 });
-	backward.PushBack({ 1193,614,74,114 });
-	backward.PushBack({ 1272,614,74,114 });
-	backward.PushBack({ 1351,614,74,114 });
-	backward.speed = 0.4f;
+	//backward.PushBack({15,614,74,114});
+	//backward.PushBack({ 94,614,74,114 });
+	//backward.PushBack({ 173,614,74,114 });
+	//backward.PushBack({ 264,610,68,118 });
+	//backward.PushBack({ 337,610,68,118 });
+	//backward.PushBack({ 410,612,68,117 });
+	//backward.PushBack({ 482,609,67,121 });
+	backward.PushBack({ 553,608,67,121 });
+	//backward.PushBack({ 624,618,82,110 });
+	//backward.PushBack({ 711,618,82,110 });
+	//backward.PushBack({ 798,618,82,110 });
+	//backward.PushBack({ 884,618,82,110 });
+	//backward.PushBack({ 971,611,69,117 });
+	//backward.PushBack({ 1045,611,69,117 });
+	//backward.PushBack({ 1119,610,69,118 });
+	//backward.PushBack({ 1193,614,74,114 });
+	//backward.PushBack({ 1272,614,74,114 });
+	//backward.PushBack({ 1351,614,74,114 });
+	backward.speed = 0.2f;
 	//jump animation
 	/*
 	jump.PushBack({ x,y,w,h });
@@ -81,13 +81,16 @@ ModulePlayer::ModulePlayer()
 	*/
 
 	//kick animation
-	kick.PushBack({ 16,137,66,95 });
+	kick.PushBack({ 16,137,66,95 }); //initial kick frames
 	kick.PushBack({ 87,137,66,95 });
 	kick.PushBack({ 158,137,66,95 });
-	kick.PushBack({ 229,134,86,98 });
+	kick.PushBack({ 229,134,86,98 }); //final kick frames
 	kick.PushBack({ 320,134,86,98 });
 	kick.PushBack({ 411,134,86,98 });
-	kick.speed =0.4f;
+	kick.PushBack({ 16,137,66,95 }); //initial kick frames
+	kick.PushBack({ 87,137,66,95 });
+	kick.PushBack({ 158,137,66,95 });
+	kick.speed =0.2f;
 }
 
 ModulePlayer::~ModulePlayer(){
