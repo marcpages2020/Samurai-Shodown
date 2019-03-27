9cbd131c74a3f1354a3a3cacdd2170dcc5200094
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-03-27 13:02:26 +0100

    fix backwards

--- Samurai_Shodown/ModulePlayer.cpp
+++ Samurai_Shodown/ModulePlayer.cpp
@@ -48,25 +48,26 @@ ModulePlayer::ModulePlayer()
 	forward.speed = 0.4f;
 
 	//backward animation
-	//backward.PushBack({15,614,74,114});
-	//backward.PushBack({ 94,614,74,114 });
-	//backward.PushBack({ 173,614,74,114 });
-	//backward.PushBack({ 264,610,68,118 });
-	//backward.PushBack({ 337,610,68,118 });
-	//backward.PushBack({ 410,612,68,117 });
-	//backward.PushBack({ 482,609,67,121 });
+	backward.PushBack({15,614,74,114});
+	backward.PushBack({ 94,614,74,114 });
+	backward.PushBack({ 173,614,74,114 });
+	backward.PushBack({ 264,610,68,118 });
+	backward.PushBack({ 337,610,68,118 });
+	backward.PushBack({ 410,612,68,117 });
+	backward.PushBack({ 482,609,67,121 });
 	backward.PushBack({ 553,608,67,121 });
-	//backward.PushBack({ 624,618,82,110 });
-	//backward.PushBack({ 711,618,82,110 });
-	//backward.PushBack({ 798,618,82,110 });
-	//backward.PushBack({ 884,618,82,110 });
-	//backward.PushBack({ 971,611,69,117 });
-	//backward.PushBack({ 1045,611,69,117 });
-	//backward.PushBack({ 1119,610,69,118 });
-	//backward.PushBack({ 1193,614,74,114 });
-	//backward.PushBack({ 1272,614,74,114 });
-	//backward.PushBack({ 1351,614,74,114 });
+	backward.PushBack({ 624,608,82,121 });
+	backward.PushBack({ 711,608,82,121 });
+	backward.PushBack({ 798,608,82,121 });
+	backward.PushBack({ 884,608,82,121 });
+	backward.PushBack({ 971,607,69,121});
+	backward.PushBack({ 1045,611,69,117 });
+	backward.PushBack({ 1119,610,69,118 });
+	backward.PushBack({ 1193,614,74,114 });
+	backward.PushBack({ 1272,614,74,114 });
+	backward.PushBack({ 1351,614,74,114 });
 	backward.speed = 0.2f;
+
 	//jump animation
 	/*
 	jump.PushBack({ x,y,w,h });
