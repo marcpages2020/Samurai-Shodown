231dd19a73ed91f92f59ed9fc84b42727f1bd3eb
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-14 17:11:48 +0200

    fixing pause

--- Samurai_Shodown/ModulePlayer2.cpp
+++ Samurai_Shodown/ModulePlayer2.cpp
@@ -206,124 +206,127 @@ bool ModulePlayer2::Start()
 
 update_status ModulePlayer2::PreUpdate()
 {
+	if (!App->is_paused) {
+		player_input2.pressing_1 = App->input->keyboard[SDL_SCANCODE_1] == KEY_REPEAT;
+		player_input2.pressing_3 = App->input->keyboard[SDL_SCANCODE_3] == KEY_REPEAT;
+		player_input2.pressing_2 = App->input->keyboard[SDL_SCANCODE_2] == KEY_REPEAT;
+		player_input2.pressing_N = App->input->keyboard[SDL_SCANCODE_N] == KEY_DOWN;
+		player_input2.pressing_M = App->input->keyboard[SDL_SCANCODE_M] == KEY_DOWN;
+		player_input2.pressing_5 = App->input->keyboard[SDL_SCANCODE_5] == KEY_DOWN;
+		player_input2.pressing_B = App->input->keyboard[SDL_SCANCODE_B] == KEY_DOWN;
+		player_input2.pressing_F5 = App->input->keyboard[SDL_SCANCODE_F5] == KEY_DOWN;
 
-	player_input2.pressing_1 = App->input->keyboard[SDL_SCANCODE_1] == KEY_REPEAT;
-	player_input2.pressing_3 = App->input->keyboard[SDL_SCANCODE_3] == KEY_REPEAT;
-	player_input2.pressing_2 = App->input->keyboard[SDL_SCANCODE_2] == KEY_REPEAT;
-	player_input2.pressing_N = App->input->keyboard[SDL_SCANCODE_N] == KEY_DOWN;
-	player_input2.pressing_M = App->input->keyboard[SDL_SCANCODE_M] == KEY_DOWN;
-	player_input2.pressing_5 = App->input->keyboard[SDL_SCANCODE_5] == KEY_DOWN;
-	player_input2.pressing_B = App->input->keyboard[SDL_SCANCODE_B] == KEY_DOWN;
-	player_input2.pressing_F5 = App->input->keyboard[SDL_SCANCODE_F5] == KEY_DOWN;
-	
-	
-	if (state2 == IDLE2) {
-		if (player_input2.pressing_1)
-			state2 = BACKWARD2;
-		if (player_input2.pressing_3)
-			state2 = FORWARD2;
-		if (player_input2.pressing_N)
-			state2 = KICK2;
-		if (player_input2.pressing_M)
-			state2 = PUNCH2;
-		if (player_input2.pressing_5)
-			state2 = JUMP2;
-		if (player_input2.pressing_2)
-			state2 = CROUCH2_DOWN;
-		if (player_input2.pressing_B)
-			state2 = TWISTER2;
-			
-	}
-	
-	if (state2 == BACKWARD2) {
-		if (!player_input2.pressing_1)
-			state2 = IDLE2;
-		if (player_input2.pressing_M)
-			state2 = PUNCH2;
-		if (player_input2.pressing_N)
-			state2 = KICK2;
-		if (player_input2.pressing_5)
-			state2 = JUMP_BACKWARD2;
-	}
-	if (state2 == FORWARD2) {
-		if (!player_input2.pressing_3)
-			state2 = IDLE2;
-		if (player_input2.pressing_M)
-			state2 = PUNCH2;
-		if (player_input2.pressing_N)
-			state2 = KICK2;
-		if (player_input2.pressing_5)
-			state2 = JUMP_FORWARD2;
-	}
-	if (state2 == KICK2) {
-		if (current_animation->Finished()) {
-			state2 = IDLE2;
-			kick.Reset();
-		}
-	}
-	if (state2 == PUNCH2) {
 
-		if (current_animation->Finished()) {
-			state2 = IDLE2;
-			punch.Reset();
+		if (state2 == IDLE2) {
+			if (player_input2.pressing_1)
+				state2 = BACKWARD2;
+			if (player_input2.pressing_3)
+				state2 = FORWARD2;
+			if (player_input2.pressing_N)
+				state2 = KICK2;
+			if (player_input2.pressing_M)
+				state2 = PUNCH2;
+			if (player_input2.pressing_5)
+				state2 = JUMP2;
+			if (player_input2.pressing_2)
+				state2 = CROUCH2_DOWN;
+			if (player_input2.pressing_B)
+				state2 = TWISTER2;
+
 		}
-	}
-	if (state2 == JUMP_NEUTRAL2)
-	{
-		if (current_animation->Finished())
+
+		if (state2 == BACKWARD2) {
+			if (!player_input2.pressing_1)
+				state2 = IDLE2;
+			if (player_input2.pressing_M)
+				state2 = PUNCH2;
+			if (player_input2.pressing_N)
+				state2 = KICK2;
+			if (player_input2.pressing_5)
+				state2 = JUMP_BACKWARD2;
+		}
+		if (state2 == FORWARD2) {
+			if (!player_input2.pressing_3)
+				state2 = IDLE2;
+			if (player_input2.pressing_M)
+				state2 = PUNCH2;
+			if (player_input2.pressing_N)
+				state2 = KICK2;
+			if (player_input2.pressing_5)
+				state2 = JUMP_FORWARD2;
+		}
+		if (state2 == KICK2) {
+			if (current_animation->Finished()) {
+				state2 = IDLE2;
+				kick.Reset();
+			}
+		}
+		if (state2 == PUNCH2) {
+
+			if (current_animation->Finished()) {
+				state2 = IDLE2;
+				punch.Reset();
+			}
+		}
+		if (state2 == JUMP_NEUTRAL2)
 		{
-			state2 = IDLE2;
-			jump_neutral2.Reset();
+			if (current_animation->Finished())
+			{
+				state2 = IDLE2;
+				jump_neutral2.Reset();
+			}
 		}
-	}
-	if (state2 == JUMP_FORWARD2)
-	{
-		if (current_animation->Finished())
+		if (state2 == JUMP_FORWARD2)
 		{
-			state2 = IDLE2;
-			jump_forward2.Reset();
+			if (current_animation->Finished())
+			{
+				state2 = IDLE2;
+				jump_forward2.Reset();
+			}
 		}
-	}
-	if (state2 == JUMP_BACKWARD2)
-	{
-		if (current_animation->Finished())
+		if (state2 == JUMP_BACKWARD2)
 		{
-			state2 = IDLE2;
-			jump_backward2.Reset();
+			if (current_animation->Finished())
+			{
+				state2 = IDLE2;
+				jump_backward2.Reset();
+			}
 		}
-	}
-	if (state2 == CROUCH2_DOWN)
-	{
-		if (!player_input2.pressing_2)
+		if (state2 == CROUCH2_DOWN)
 		{
-			state2 = CROUCH2_UP;
-			crouch2_down.Reset();
+			if (!player_input2.pressing_2)
+			{
+				state2 = CROUCH2_UP;
+				crouch2_down.Reset();
+			}
 		}
-	}
-	if (state2 == CROUCH2_UP)
-	{
-		if (current_animation->Finished()) {
-			state2 = IDLE2;
-			crouch2_up.Reset();
-		}
-	}
-	if (state2 == TWISTER2)
-	{
-		if (current_animation->Finished())
+		if (state2 == CROUCH2_UP)
 		{
-			state2 = IDLE2;
-			twister.Reset();
-			is_tornado_created2 = false;
+			if (current_animation->Finished()) {
+				state2 = IDLE2;
+				crouch2_up.Reset();
+			}
 		}
+		if (state2 == TWISTER2)
+		{
+			if (current_animation->Finished())
+			{
+				state2 = IDLE2;
+				twister.Reset();
+				is_tornado_created2 = false;
+			}
+		}
+		if ((player_input2.pressing_F5) && (collider_player2 != nullptr)) {
+			collider_player2->to_delete = true;
+			collider_player2 = nullptr;
+		}
+		else if ((player_input2.pressing_F5) && (collider_player2 == nullptr))
+		{
+			collider_player2 = App->collision->AddCollider({ 0, 0,71,95 }, COLLIDER_PLAYER_2, (Module*)App->player);
+		}
+
 	}
-	if ((player_input2.pressing_F5) && (collider_player2 != nullptr)) {
-		collider_player2->to_delete = true;
-		collider_player2 = nullptr;
-	}
-	else if ((player_input2.pressing_F5) && (collider_player2 == nullptr))
-	{
-		collider_player2 = App->collision->AddCollider({ 0, 0,71,95 }, COLLIDER_PLAYER_2, (Module*)App->player);
-	}
+	
 	return UPDATE_CONTINUE;
 }
 
@@ -331,85 +334,88 @@ update_status ModulePlayer2::PreUpdate()
 update_status ModulePlayer2::Update()
 {
 	lposition = position;
-	switch (state2)
-	{
-	case IDLE2:
-		current_animation = &idle;
-		position.y = initial_y;
-		break;
-	case PUNCH2:
-		current_animation = &punch;
-		break;
-	case JUMP_NEUTRAL2:
-		current_animation = &jump_neutral2;
-		position.y -= speed * 1.75 * mult;
-		if (position.y <= 120) {
-			mult = -1;
-		}
-		else if (position.y == initial_y)
+	if (!App->is_paused) {
+		switch (state2)
 		{
-			mult = 1;
-			jump_neutral2.Reset();
-			state2 = IDLE2;
-		}
-		break;
-	case JUMP_FORWARD2:
-		current_animation = &jump_forward2;
-		position.y -= speed * 1.75 * mult;
-		position.x += speed;
-		if (position.y <= 120) {
-			mult = -1;
-		}
-		else if (position.y == initial_y)
-		{
-			mult = 1;
-			jump_forward2.Reset();
-			state2 = IDLE2;
-		}
-		break;
-	case JUMP_BACKWARD2:
-		current_animation = &jump_backward2;
-		position.y -= speed * 1.75 * mult;
-		position.x -= speed;
-		if (position.y <= 120) {
-			mult = -1;
-		}
-		else if (position.y == initial_y)
-		{
-			mult = 1;
-			jump_backward2.Reset();
-			state2 = IDLE2;
-		}
-		break;
-	case KICK2:
-		current_animation = &kick;
-		break;
-	case FORWARD2:
-		current_animation = &forward;
-		position.x += speed;
-		break;
-	case BACKWARD2:
-		current_animation = &backward;
-		position.x -= speed;
-		break;
-	case CROUCH2_DOWN:
-		current_animation = &crouch2_down;
-		break;
-	case CROUCH2_UP:
-		current_animation = &crouch2_up;
-		break;
-	case TWISTER2:
-		current_animation = &twister;
-		/*if (current_animation->SeeCurrentFrame() == 10 && !is_tornado_created2) {
+		case IDLE2:
+			current_animation = &idle;
+			position.y = initial_y;
+			break;
+		case PUNCH2:
+			current_animation = &punch;
+			break;
+		case JUMP_NEUTRAL2:
+			current_animation = &jump_neutral2;
+			position.y -= speed * 1.75 * mult;
+			if (position.y <= 120) {
+				mult = -1;
+			}
+			else if (position.y == initial_y)
+			{
+				mult = 1;
+				jump_neutral2.Reset();
+				state2 = IDLE2;
+			}
+			break;
+		case JUMP_FORWARD2:
+			current_animation = &jump_forward2;
+			position.y -= speed * 1.75 * mult;
+			position.x += speed;
+			if (position.y <= 120) {
+				mult = -1;
+			}
+			else if (position.y == initial_y)
+			{
+				mult = 1;
+				jump_forward2.Reset();
+				state2 = IDLE2;
+			}
+			break;
+		case JUMP_BACKWARD2:
+			current_animation = &jump_backward2;
+			position.y -= speed * 1.75 * mult;
+			position.x -= speed;
+			if (position.y <= 120) {
+				mult = -1;
+			}
+			else if (position.y == initial_y)
+			{
+				mult = 1;
+				jump_backward2.Reset();
+				state2 = IDLE2;
+			}
+			break;
+		case KICK2:
+			current_animation = &kick;
+			break;
+		case FORWARD2:
+			current_animation = &forward;
+			position.x += speed;
+			break;
+		case BACKWARD2:
+			current_animation = &backward;
+			position.x -= speed;
+			break;
+		case CROUCH2_DOWN:
+			current_animation = &crouch2_down;
+			break;
+		case CROUCH2_UP:
+			current_animation = &crouch2_up;
+			break;
+		case TWISTER2:
+			current_animation = &twister;
+			/*if (current_animation->SeeCurrentFrame() == 10 && !is_tornado_created2) {
 			App->particles->AddParticle(App->particles->tornado, position.x + 50, position.y - 205, COLLIDER_PLAYER_PARTICLES);
 			is_tornado_created2 = true;
 			collider_player_particles2 = App->collision->AddCollider({ position.x + 50,position.y - 205,83,207 }, COLLIDER_PLAYER_PARTICLES, (Module*)App->player);
-		}*/
-		break;
-	default:
-		LOG("No state found :(");
-		break;
+			}*/
+			break;
+		default:
+			LOG("No state found :(");
+			break;
+		}
 	}
+	
 	
 	//Draw everything
 	SDL_Rect r = current_animation->GetCurrentFrame();
