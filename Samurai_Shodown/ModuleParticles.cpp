9b1ec771200dd5a39356142efb2e1d690920ed13
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-05-20 12:23:45 +0200

    player 2 special attack fixed

--- Samurai_Shodown/ModuleParticles.cpp
+++ Samurai_Shodown/ModuleParticles.cpp
@@ -205,15 +205,16 @@ bool Particle::Update()
 	
 	if (!App->is_paused)
 	{
-			position.y += speed.y;
 		
 		if (coll->type == COLLIDER_PLAYER_PARTICLES)
 		{
 			position.x += speed.x;
+			position.y += speed.y;
 		}
 		else if (coll->type == COLLIDER_PLAYER_2_PARTICLES)
 		{
 			position.x -= speed.x;
+			position.y += speed.y;
 		}
 	}
 
