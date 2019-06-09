10fdb0c912c72be505364404a61e6b59a76ecf11
Pablo <eduinvestor100@gmail.com>
2019-06-09 13:21:02 +0200

    special attack flip started

--- Samurai_Shodown/ModulePlayer2.cpp
+++ Samurai_Shodown/ModulePlayer2.cpp
@@ -3024,35 +3024,42 @@ bool ModulePlayer2::checkSpecialAttack() {
 	int j = lastInput;//<- Change this
 	int done = 0; //If done = 3. Special attack = true 
 
-	while (i < 100) {
-		switch (done) {
-		case 0:
-			if (inputs[j] == 's')//down/crouch
-				done++;
-			break;
-		case 1:
-			if (inputs[j] == 'd' || 's')//down and forward
-				done++;
-			break;
-		case 2:
-			if (inputs[j] == 'd')//forward
-				done++;
-			break;
-		case 3:
-			if (inputs[j] == 'h') //punch				
-				return true;
-		default:
-			//Special attack is false
-			return false;
+	if (flip == SDL_FLIP_HORIZONTAL) {
+		while (i < 100) {
+			switch (done) {
+			case 0:
+				if (inputs[j] == 's')//down/crouch
+					done++;
+				break;
+			case 1:
+				if (inputs[j] == 'd' || 's')//down and forward
+					done++;
+				break;
+			case 2:
+				if (inputs[j] == 'd')//forward
+					done++;
+				break;
+			case 3:
+				if (inputs[j] == 'h') //punch				
+					return true;
+			default:
+				//Special attack is false
+				return false;
+			}
+
+			if (j < 99)
+				j++;
+			else
+				j = 0;
+
+			i++;
 		}
-
-		if (j < 99)
-			j++;
-		else
-			j = 0;
-
-		i++;
 	}
+	else {
+
+	}
+
+	
 	return false;
 }
 void ModulePlayer2::newInput(char newInput) {
